a7e94ac9c4c11f65df7526397496480fdb6086d1
Stevodeveloper <stepheneoduor20@gmail.com>
2022-08-10 23:03:10 +0000

    super simple shell

--- main.h
+++ main.h
@@ -1,156 +1,137 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#define BUFFSIZE 1024
 
-#include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include <fcntl.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
 
+#define END_OF_FILE -2
+#define EXIT -3
+
+/* Global environemnt */
 extern char **environ;
+/* Global program name */
+char *name;
+/* Global history counter */
+int hist;
 
 /**
- * serverEnv_s - environment variable
- * @PathName: environment name 
- * @widepath: global path
- * @next: points to the next node
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
  */
-
-typedef struct serverEnv_s
+typedef struct list_s
 {
-char *pathName;
-char *value;
-char *globPath;
-struct serverEnv_s *next;
-} serverEnv_t;
+	char *dir;
+	struct list_s *next;
+} list_t;
 
 /**
- * server_s - stores data variable
- * @cmdName: command name
- * @cmdList: command list
- * @argument: argument's arrays
- * @buff: buffer
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
  */
-
-typedef struct server_s
+typedef struct builtin_s
 {
-char *cmdName;
-char **cmdList;
-char *pgName;
-char **history;
-char *buff;
-char **argument;
-serverEnv_t *env;
-} server_t;
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
 
 /**
- * Error - a structure for error message
- * @input: error input
- * @errorMessage: pointer to error messange
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
  */
-
-typedef struct Error
+typedef struct alias_s
 {
-int input;
-char *errorMessage;
-} Error_t;
+	char *name;
+	char *value;
+	struct alias_s *next;
+} alias_t;
 
-/**
- * convert_s - convert a structure to a function
- * @commandName: input string
- * @function: pointer to a function
- */
-typedef struct convert_s
-{
+/* Global aliases linked list */
+alias_t *aliases;
 
-char *cmdName;
-void (*func)(server_t *);
+/* Main Helpers */
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_strtok(char *line, char *delim);
+char *get_location(char *command);
+list_t *get_path_dir(char *path);
+int execute(char **args, char **front);
+void free_list(list_t *head);
+char *_itoa(int num);
 
-} convert_t;
+/* Input Helpers */
+void handle_line(char **line, ssize_t read);
+void variable_replacement(char **args, int *exe_ret);
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+void free_args(char **args, char **front);
+char **replace_aliases(char **args);
 
-void print_prompt(void);
-int _write(char *ptrstr);
-int _putchar(char c);
-int main(int argc __attribute__((unused)), char **argv);
-void _getline(server_t *strucptr);
-void (*getFunc(char *cmd))(server_t *);
-void _execve(server_t *ptrData);
+/* String functions */
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
-void *_calloc(unsigned int n, unsigned int size);
-char *_memset(char *str, char c, unsigned int n_Byte);
-char *_memcpy(char *dest, char *src, unsigned int size);
-void *_realloc(void *ptr, unsigned int size, unsigned int n); 
-serverEnv_t *_AddNodeEnd(serverEnv_t **HeadNode, char *globvalue);
+/* Builtins */
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
-void _Envserver(server_t *ptr);
-serverEnv_t *_CreateNode(char *glob);
-serverEnv_t *_LastNode(serverEnv_t *Node);
-server_t *_countvalue(char **str);
-int _Arr(char c, char *array);
-int wordNum(char *str, char *aps);
-char **_strtow(char *str, char *ap, char *sep);
-int _checker(char c, char *E_sep);
-int _checksep(char c, char *sep);
+/* Builtin Helpers */
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
 
-char *_strcat(char *dest, char *src);
-int _strcmp(char *str1, char *str2);
-int _strlen(char *str);
-char *_strdup(char *str);
-char *_strcpy(char *dest, char *src);
-char *_strconcat(char *Str1, char *Str2);
+/* Error Handling */
+int create_error(char **args, int err);
+char *error_env(char **args);
+char *error_1(char **args);
+char *error_2_exit(char **args);
+char *error_2_cd(char **args);
+char *error_2_syntax(char **args);
+char *error_126(char **args);
+char *error_127(char **args);
 
-void _printEnviron(serverEnv_t *_Env);
-void _Environ(server_t *ptr);
-serverEnv_t *_getenv(serverEnv_t *ptrEnv, char *ptrName);
-char *_EnvName(char *ptrvar);
-char *EnvValue(char *ptrvar);
-void _setEnviron(serverEnv_t *ptrEnv, char *ptrName, char *ptrV, int ptrOW);
-void _cDir(server_t *ptrData);
-void _cDHome(server_t *chptr, char *ptrcd);
-void _cD_prev(server_t *ptrD, char *ptrCurDir);
-void _RandomDir(server_t *varptr, char *curDir);
-char *_genWord(char *Cptr, int abc, int len);
-void _Word(char *ptrW, int *ptrI, char **Arr);
-char *_cleanString(char *prmString);
-void _Error(server_t *ptrData, int m);
+/* Linkedlist Helpers */
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
 
-void freeServerData(server_t *ptrData);
-void freeArgument(char **ptr);
-void freeList(serverEnv_t *ptrHeadNode);
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
 
-void _help(server_t *ptrData);
-void _exits(server_t *ptrData);
-int _listEnvLen(serverEnv_t *prmHead);
-void _unsetEnvironment(server_t *ptrserv);
-void _unsetEnviron(server_t *ptrserv, char *ptrName);
-int _delete(serverEnv_t *ptrH, char *ptrN);
-serverEnv_t *_IndexNode(
-	serverEnv_t *prmHead,
-	unsigned int prmIndex
-);
-int _isNumber(char *s);
-char *_itoa(int ptrNum);
-int _isdigit(char ptrC);
-int _numLen(int Num);
-int _atoi(char *ptrstr);
+int proc_file_commands(char *file_path, int *exe_ret);
 
-int _EnvIndex(serverEnv_t *prmHead, char *prmName);
-char *_genGlobal(char *ptrName, char *ptrV);
-char *_absolute(server_t *ptrData);
-char **_parse(server_t *ptrData);
-char *_genAbs(char *ptrPath, char *ptrCmdName);
-
-void _changedirectoryHelp(void);
-void _envHelp(void);
-void _exitHelp(void);
-void _helpHelp(void);
-void _defaultHelp(char *ptrCmd);
-void _setEnvironment(server_t *ptrData);
-
-#endif/*MAIN_H*/
+#endif
